58e3381e6d875871261bae1d7c5c9421e55d2aae
Daisuke Kasahara <daiponpon0211@gmail.com>
2017-11-21 23:47:03 +0900

    second commit

--- src/main/java/com/kasahara/App.java
+++ src/main/java/com/kasahara/App.java
@@ -4,10 +4,19 @@ package com.kasahara;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        new App().printMessage("Taro");
+    }
+
+    public String getMessage(String name){
+        return "Hi," + name + ". Welcome to Maven World!";
+    }
+
+    public void printMessage(String name) {
+        System.out.println("¥n***** App class *****¥n");
+        System.out.println(this.getMessage(name));
+        System.out.println("¥n***** App class *****¥n");
     }
 }
+
--- src/test/java/com/kasahara/AppTest.java
+++ src/test/java/com/kasahara/AppTest.java
@@ -7,32 +7,37 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
+
+    private App app = null;
+
+    public AppTest( String testName ) {
         super( testName );
+        app = new App();
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite( AppTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertNotNull( app );
+    }
+
+    public void testGetMessage(){
+        String name = "hoge";
+        String msg = "Hi," + name + ". Welcome to Maven World!";
+        assertEquals(app.getMessage("hoge"), msg);
     }
 }
