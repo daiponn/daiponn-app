55bd12e44cb45a0f64e8238c2ecd16d7481dd734
Daisuke Kasahara <daiponpon0211@gmail.com>
2017-11-23 00:39:46 +0900

    daiponn-libとの連携

--- src/main/java/com/kasahara/App.java
+++ src/main/java/com/kasahara/App.java
@@ -1,22 +1,28 @@
 package com.kasahara;
 
+import com.kasahara.lib.Lib;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        new App().printMessage("Taro");
-    }
-
-    public String getMessage(String name){
-        return "Hi," + name + ". Welcome to Maven World!";
-    }
-
-    public void printMessage(String name) {
-        System.out.println("¥n***** App class *****¥n");
-        System.out.println(this.getMessage(name));
-        System.out.println("¥n***** App class *****¥n");
-    }
+//    public static void main( String[] args ) {
+//        new App().printMessage("Taro");
+//    }
+//
+//    public String getMessage(String name){
+//        return "Hi," + name + ". Welcome to Maven World!";
+//    }
+//
+//    public void printMessage(String name) {
+//        System.out.println("¥n***** App class *****¥n");
+//        System.out.println(this.getMessage(name));
+//        System.out.println("¥n***** App class *****¥n");
+//    }
+      public static void main( String[] args ) {
+          Lib lib = new Lib("Sachiko", "I am Happy");
+          System.out.println(lib.getMessage());
+      }
 }
 
--- src/test/java/com/kasahara/AppTest.java
+++ src/test/java/com/kasahara/AppTest.java
@@ -14,30 +14,43 @@ public class AppTest extends TestCase {
      * @param testName name of the test case
      */
 
-    private App app = null;
+//    private App app = null;
+//
+//    public AppTest( String testName ) {
+//        super( testName );
+//        app = new App();
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite() {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp() {
+//        assertNotNull( app );
+//    }
+//
+//    public void testGetMessage(){
+//        String name = "hoge";
+//        String msg = "Hi," + name + ". Welcome to Maven World!";
+//        assertEquals(app.getMessage("hoge"), msg);
+//    }
 
     public AppTest( String testName ) {
         super( testName );
-        app = new App();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite( AppTest.class );
+    public static Test suite(){
+        return new TestSuite( AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertNotNull( app );
-    }
-
-    public void testGetMessage(){
-        String name = "hoge";
-        String msg = "Hi," + name + ". Welcome to Maven World!";
-        assertEquals(app.getMessage("hoge"), msg);
+    public void testApp(){
+        App app = new App();
+        assertNotNull(app);
     }
 }
